3293d61b88a2f51f22240e4b413ab922bd29db2a
Christopher Bitti <christopherbitti9@gmail.com>
2025-11-26 11:21:04 -0500

    DB layer

---
+++ app/models/pokemon_record.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlalchemy import String, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.base_class import Base
+
+
+class PokemonRecord(Base):
+    __tablename__ = "pokemon_records"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    pokemon_name: Mapped[str] = mapped_column(String, index=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
